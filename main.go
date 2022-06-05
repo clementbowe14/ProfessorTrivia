7573bc87985fb66f8dc7dfe4b6e794b345cfa4ea
Clement Bowe <cbowe023@fiu.edu>
2022-06-05 17:27:00 -0400

    automated adding the environment variables for professor trivia

--- main.go
+++ main.go
@@ -1,36 +1,75 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
-	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	Token string
-)
+type Config struct {
+	BotToken string `json:"botToken"`
+	ClientID string `json:"clientID"`
+}
 
-//Note BotToken will be set as an environment variable
-var (
-	GuildID  = flag.String("guild", "", "Test guild ID")
-	BotToken = flag.String("token", "", "Bot token")
-)
+var token string
+var conf Config
 
 func init() {
-	flag.Parse()
+
+	// flag.StringVar(&token, "token", "", "Bot token")
+	// flag.StringVar(&message, "message", "", "This is the first message: ")
+	// flag.Parse();
+	file, err := os.Open("env-config.json")
+	if err != nil {
+		panic(err)
+	}
+
+	jsonParser := json.NewDecoder(file)
+	err = jsonParser.Decode(&conf)
+
+	if err != nil {
+		panic("there was an error while trying to decode the config file")
+	}
+	token = "Bot token" + conf.BotToken
 }
 
 func main() {
 
-	//initialize the discord bot
-	s, err := discordgo.New("Bot " + *BotToken)
-
-	if err != nil {
-		log.Fatalf("Looks like you entered the wrong parameters for your bot")
+	//check if token provided
+	if token == "" {
+		panic("Missing token")
 	}
 
-	fmt.Println(s)
+	dg, err := discordgo.New("Bot " + conf.BotToken)
+	if err != nil {
+		panic("Discord go was not able to create your session")
+	}
 
+	//add handler
+	dg.AddHandler(discordMessageCreate)
+
+	//open a websocket connection to discord
+	err = dg.Open()
+	defer dg.Close()
+
+	fmt.Println("You now have a socket connected to the server!")
+
+	//runs process until interrupted by ctrl + c or contains an invalid command
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT)
+	<-sc
+
+	if err != nil {
+		panic("Could not connect to discord")
+	}
+
+}
+
+//listens to when a message is created
+func discordMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println("New Message!")
 }
